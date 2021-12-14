12c4efbe7775350207e50fae4a73264e6d94164f
Елисей Ехлаков <elisey.ekhlakov@krit.pro>
2021-12-14 16:53:18 +0400

    +Factory, Singleton

--- app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -1,26 +1,29 @@
 package com.example.dependencyinjectionstart.example2.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dependencyinjectionstart.R
+import com.example.dependencyinjectionstart.example2.ExampleApp
+import com.example.dependencyinjectionstart.example2.data.database.ExampleDatabase
 import com.example.dependencyinjectionstart.example2.di.DaggerApplicationComponent
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var viewModel: ExampleViewModel
+    lateinit var viewModel : ExampleViewModel
 
     private val component by lazy {
-        DaggerApplicationComponent
-            .factory()
-            .create(application, System.currentTimeMillis())
+        (application as ExampleApp).component
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         component.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         viewModel.method()
     }
 }
