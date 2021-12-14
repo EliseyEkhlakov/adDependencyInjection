bec7e6816011ba6a32bd30f868d530e482bdf2d5
Елисей Ехлаков <elisey.ekhlakov@krit.pro>
2021-12-14 17:37:42 +0400

    +Custom Scope

--- app/src/main/java/com/example/dependencyinjectionstart/example2/di/DataModule.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/di/DataModule.kt
@@ -12,9 +12,11 @@ import dagger.Provides
 @Module
 interface DataModule {
 
+    @ApplicationScope
     @Binds
     fun bindLocalDataSource(impl: ExampleLocalDataSourceImpl): ExampleLocalDataSource
 
+    @ApplicationScope
     @Binds
     fun bindRemoteDataSource(impl: ExampleRemoteDataSourceImpl): ExampleRemoteDataSource
 
